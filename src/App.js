5f3e281fad35cabd620e1db37c99a69a19d870bf
danilo-barbosa <danilo_barbosa@hotmail.com>
2019-10-18 12:54:15 -0300

    ritmo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cassinooooo!
+          O ritmo das baladaaaas!
         </p>
         <a
           className="App-link"
